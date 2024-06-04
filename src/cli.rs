9b940becb877aef1ad8b278f743d627677c0c5d9
NoLoM-e <dyjey18@gmail.com>
2024-06-04 14:18:02 +0300

    Created argument parcing with structopt lib

---
+++ src/cli.rs
@@ -0,0 +1,33 @@
+use std::path::PathBuf;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+enum Action {
+    /// Add task to journal
+    Add {
+        /// Task description
+        #[structopt()]
+        task: String,
+    },
+    /// Mark task with number as Done and remove from journal
+    Done {
+        /// Posion of taks to remove
+        #[structopt()]
+        id: usize,
+    },
+    /// List all tasks in journal
+    List,
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(
+    name = "My journal",
+    about = "A command line tool to handle todo tasks in journals"
+)]
+pub struct CliArgs {
+    #[structopt(subcommand)]
+    action: Action,
+    #[structopt(parse(from_os_str), short, long)]
+    journal_path: Option<PathBuf>,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use cli::CliArgs;
+use structopt::StructOpt;
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+    let app_args = CliArgs::from_args();
+    println!("{:?}", app_args);
 }
